527a16c96b08e4868a8f769ecbe4f18e470dae02
royhan31 <izzapark34@gmail.com>
2020-10-05 02:59:47 +0700

    Updates

---
+++ precache-manifest.19fbbbec7ee606277e2f92e9eedd7467.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d46d7c7503e74d535c2be9dc4bb8a30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcf38152a30d64344dc",
+    "url": "/static/js/2.ea16a39c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea16a39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a790ce2b5c8d32a46a",
+    "url": "/static/js/main.0d56c032.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "43651ced52b01d4df046ddcde0b8159f",
+    "url": "/static/media/Izzatur Royhan.43651ced.pdf"
+  },
+  {
+    "revision": "be267eed4fab1a3693c8923c1cfd9a0e",
+    "url": "/static/media/corona.be267eed.png"
+  },
+  {
+    "revision": "c978b466484ecebdae5f963b9eafd2e1",
+    "url": "/static/media/fendap-1.c978b466.jpg"
+  },
+  {
+    "revision": "05901fc551724cfeef4d2ec1b8a47a04",
+    "url": "/static/media/fendap-2.05901fc5.jpg"
+  },
+  {
+    "revision": "9327419fa11a8aa1c2c12f3571f0d1ba",
+    "url": "/static/media/fendap-web.9327419f.png"
+  },
+  {
+    "revision": "a2521791099281c59336768afdd2cc0e",
+    "url": "/static/media/roy.a2521791.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5be06751fe8971f3250e949f8d456ba.js"
+  "/precache-manifest.19fbbbec7ee606277e2f92e9eedd7467.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
